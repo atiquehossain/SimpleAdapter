755b819dcb1b5e1b6e4951572bb971c93bac80fd
Atique <atique.hossain@savethechildren.org>
2021-07-29 12:25:01 +0600

    comment & done

--- app/src/main/java/com/example/simpleadapter/MainActivity.java
+++ app/src/main/java/com/example/simpleadapter/MainActivity.java
@@ -25,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayList<HashMap<String,String>>arrayList= new ArrayList<>();
         listView=findViewById(R.id.listview);
+//We take Arraylist to put the value
+        ArrayList<HashMap<String,String>>arrayList= new ArrayList<>();
 
 
+//loop for put the value one by one in HashMap
         for(int i=0;i<name.length;i++){
             HashMap<String,String >hashMap=new HashMap<>();
             hashMap.put("Name",name[i]);
@@ -37,7 +38,11 @@ public class MainActivity extends AppCompatActivity {
             arrayList.add(hashMap);
 
         }
+
+        //from: A list of column names that will be added to the Map associated with each item.
         String[] from={"Name","Image"};
+        //to: The views that should display column in the “from” parameter. These should all be TextViews.
+        // The first N views in this list are given the values of the first N columns in the from parameter.
         int []to={R.id.textview,R.id.imageview};
 
         SimpleAdapter simpleAdapter=new SimpleAdapter(this,arrayList,R.layout.activity_list,from,to);
